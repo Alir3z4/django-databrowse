389ed594a2f9787d449c13ae1320770ff1c6402c
Alireza Savand <alireza.savand@gmail.com>
2013-01-10 12:27:30 +0400

    Okay, Indentions with objects.py is done now ;)
    I guess this branch is done now and should be merge into master

--- django_databrowse/plugins/objects.py
+++ django_databrowse/plugins/objects.py
@@ -6,9 +6,21 @@ import urlparse
 
 class ObjectDetailPlugin(DatabrowsePlugin):
     def model_view(self, request, model_databrowse, url):
-        # If the object ID wasn't provided, redirect to the model page, which is one level up.
+        # If the object ID wasn't provided, redirect to the model page,
+        # Which is one level up.
         if url is None:
-            return http.HttpResponseRedirect(urlparse.urljoin(request.path, '../'))
-        easy_model = EasyModel(model_databrowse.site, model_databrowse.model)
+            return http.HttpResponseRedirect(
+                urlparse.urljoin(request.path, '../')
+            )
+        easy_model = EasyModel(
+            model_databrowse.site,
+            model_databrowse.model
+        )
         obj = easy_model.object_by_pk(url)
-        return render_to_response('databrowse/object_detail.html', {'object': obj, 'root_url': model_databrowse.site.root_url})
+        return render_to_response(
+            'databrowse/object_detail.html',
+            {
+                'object': obj,
+                'root_url': model_databrowse.site.root_url
+            }
+        )
