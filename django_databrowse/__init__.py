baaf0f401d5d33f974eb8ebefca3713a0b0456fe
Alireza Savand <alireza.savand@gmail.com>
2013-06-10 11:11:57 +0400

    Bump version and update/cleanup on setup.py

--- django_databrowse/__init__.py
+++ django_databrowse/__init__.py
@@ -1,4 +1,4 @@
 from django_databrowse.sites import (DatabrowsePlugin, ModelDatabrowse,
                                      DefaultModelDatabrowse, site)
 
-__version__ = (1,1)
+__version__ = (1, 2)
--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from distutils.core import setup
 
-MY_NAME = 'Alireza Savand'
-MY_EMAIL = 'alireza.savand@gmail.com'
-
 setup(
     name='django-databrowse',
-    version='1.1',
+    version='1.2',
     packages=['django_databrowse', 'django_databrowse.plugins'],
     package_dir={'django_databrowse': 'django_databrowse'},
-    package_data={'django_databrowse': 
-        ['templates/databrowse/*.html','templates/databrowse/include/*.html']},
+    package_data={
+        'django_databrowse': [
+            'templates/databrowse/*.html',
+            'templates/databrowse/include/*.html'
+        ]
+    },
     provides=['django_databrowse'],
     include_package_data=True,
     url='http://pypi.python.org/pypi/django-databrowse',
     license=open('LICENSE').read(),
-    author=MY_NAME,
-    author_email=MY_EMAIL,
-    maintainer=MY_NAME,
-    maintainer_email=MY_EMAIL,
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
     description='Databrowse is a Django application that lets you browse your data.',
     long_description=open('README.rst').read(),
-    install_requires=['django',],
+    install_requires=['django', ],
     keywords=[
         'django',
         'web',
@@ -36,7 +35,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development'
     ],
