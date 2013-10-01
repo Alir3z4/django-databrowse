4f129923c54c6995f3aa3848fc8a2570baba6d78
Yohann Gabory <yohann.gabory@outscale.com>
2013-10-02 01:59:46 +0200

    tests now lies in tests dir

---
+++ django_databrowse/tests/__init__.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class SomeModel(models.Model):
+    some_field = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.some_field
+
+    class Meta:
+        db_table = "somemodel"
+
+
+class SomeOtherModel(models.Model):
+    some_other_field = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.some_other_field
+
+
+class YetAnotherModel(models.Model):
+    yet_another_field = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.yet_another_field
+
+
+class SomeInheritedModel(SomeModel):
+    special = models.CharField(max_length=30)
+
+from datastructures import *
