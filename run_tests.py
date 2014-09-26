c136e3d6de874209106a93c7582b70998cd8820a
Alireza Savand <alireza.savand@gmail.com>
2014-09-26 04:16:19 +0400

    Fix running tests with Django +1.7

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import django
 from django.conf import settings
-from django.conf.urls import patterns, include, url
 
+
+DJANGO_VERSION = float('.'.join([str(i) for i in django.VERSION[0:2]]))
 DIR_NAME = os.path.dirname(__file__)
+
 settings.configure(
     DEBUG=True,
     DATABASES={
@@ -18,13 +21,15 @@ settings.configure(
         'django.contrib.admin',
         'django_databrowse'
     ),
-
+    MIDDLEWARE_CLASSES=[],
     ROOT_URLCONF='test_urls'
 )
 
-
-
 from django.test.simple import DjangoTestSuiteRunner
+
+if DJANGO_VERSION >= 1.7:
+    django.setup()
+
 test_runner = DjangoTestSuiteRunner(verbosity=1)
 failures = test_runner.run_tests(['django_databrowse', ])
 
