47a5144bb1e3d6c8e9f8f75d4e5bf56a0d0fdc1f
Alireza Savand <alireza.savand@gmail.com>
2017-03-16 13:57:13 +0400

    Set version to latest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-databrowse',
-    version='2016.2.26',
+    version='2016.3.16',
     packages=['django_databrowse', 'django_databrowse.plugins'],
     package_dir={'django_databrowse': 'django_databrowse'},
     package_data={
