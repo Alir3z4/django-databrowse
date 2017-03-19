edaca7a85e04451146e2233b12d48be35106820f
Alireza Savand <alireza.savand@gmail.com>
2017-03-20 02:49:38 +0400

    Add Python 3 classifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,13 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development'
     ],
 )
